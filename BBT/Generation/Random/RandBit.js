e63811e4e1c64c0a5afb1d25f28aa2cdd3c742b2
Fernando Quinonez <fernando.andres.quinonez.granados@gmail.com>
2015-11-03 23:31:30 -0500

    Some changes on public methods

--- BBT/Generation/Random/RandBit.js
+++ BBT/Generation/Random/RandBit.js
@@ -1,16 +1,28 @@
 /* 
    +----------------------------------------------------------------------+
-   |                            HEP Random                                |
-   |                      --- RandBit ---                           |
+   |                            BBT Random                                |
+   |                           --- RandBit ---                            |
    |                            Module File                               |
    +----------------------------------------------------------------------+
   
+   Module defining methods for shooting Flat or Bit random numbers, 
+   double or integers.
+   It provides methods to fill with double flat values arrays of
+   specified size, as well as methods for shooting sequences of 0,1 (bits).
   
-
+  
    +----------------------------------------------------------------------+
    | C++                                                                  |
    +----------------------------------------------------------------------+
 
+   This is derived from RandFlat and is a drop-in replacement.  However
+   the shootBit() and fireBit() methods are stateless (which makes them
+   an order of magnitude slower, but allows save/restore engine status
+   to work correctly).
+
+   M Fischler     - Created from RandFlat.cc, deleting almost all the 
+                    content since inheritance takes care of it.  2/15/00
+   M Fischler     - put and get to/from streams 12/10/04
   
 
    +----------------------------------------------------------------------+
@@ -25,8 +37,8 @@
 define( [], function(){
   "use strict";
 
-  function RandBit( ){
-
+  function RandBit( engine, ){
+    this.fEngine = engine;
   } 
 
 
@@ -37,6 +49,21 @@ define( [], function(){
     constructor: RandBit,
 
 
+    /*FIXME FIXME FIXME FIXME FIXME FIXME FIXME 
+    FIRST IMPLEMENT RandFlat.js
+    */
+    FireEndPoints: function( a, b ){
+      return ( ( b - a ) * this.fEngine.Flat() + a );
+    },
+
+    FireBit: function(){
+      var x = FireEndPoints( 0, 1 );
+      return ( ( x > 0.5 )? 1: 0 );
+    },
+
+    FireWidth: function( width ){
+      return ( width * this.fEngine.Flat() );
+    },
 
 
 
