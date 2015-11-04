00b48bfa539a5037f9bd17a9bb84af8b735b9393
Fernando Quinonez <fernando.andres.quinonez.granados@gmail.com>
2015-11-03 23:31:39 -0500

    Some changes on public methods

--- BBT/Generation/Random/RandFlat.js
+++ BBT/Generation/Random/RandFlat.js
@@ -1,16 +1,31 @@
 /* 
    +----------------------------------------------------------------------+
-   |                            HEP Random                                |
-   |                      --- RandFlat ---                           |
+   |                            BBT Random                                |
+   |                          --- RandFlat ---                            |
    |                            Module File                               |
    +----------------------------------------------------------------------+
   
+   Module defining methods for shooting flat random numbers, double or
+   integers.
+   It provides methods to fill with double flat values arrays of
+   specified size, as well as methods for shooting sequences of 0,1 (bits).
   
 
    +----------------------------------------------------------------------+
    | C++                                                                  |
    +----------------------------------------------------------------------+
 
+   Part of Geant4 (simulation toolkit for HEP).
+
+   Gabriele Cosmo - Created: 5th September 1995
+   Peter Urban    - ShootBit() and related stuff added: 5th Sep 1996
+   Gabriele Cosmo - Added operator() and additional methods to fill
+                    arrays specifying boundaries: 24th Jul 1997 
+   J.Marraffino   - Added default arguments as attributes and
+                    operator() with arguments: 16th Feb 1998
+   M. Fischler    - Moved copy constructor to protected so that
+  		    Rderived RandBit can get at it.
+   M Fischler      - put and get to/from streams 12/10/04
   
 
    +----------------------------------------------------------------------+
@@ -22,11 +37,19 @@
 
 */
 
-define( [], function(){
+define([ 
+  '../BBT/Generation/Random/JamesRandom' 
+], function(
+  JamesRandom
+)
+{
   "use strict";
 
-  function RandFlat( ){
-
+  function RandFlat( args ){
+    this.fA = args.a || 0;
+    this.fB = args.b || 1;
+    this.fWidth = args.width || ( args.b - args.a );
+    this.fEngine = args.engine || Object.create( JamesRandom() );
   } 
 
 
@@ -36,9 +59,15 @@ define( [], function(){
   RandFlat.prototype = {
     constructor: RandFlat,
 
+    Fire: function(){
+      return ( this.fB - this.fA ) * fEngine.Flat() + this.fA;
+    },
 
-
-
+    FireArray: function( /* size of vect */ size, /* Array */ vect ){
+      for( var i = 0; i < size; ++i ){
+        vect.push( this.Fire() );
+      }
+    }
 
   }
 
