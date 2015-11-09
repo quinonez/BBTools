ceaf03b6a1f736e7c7e7fdcc545953bd3e53d12a
Fernando Quinonez <fernando.andres.quinonez.granados@gmail.com>
2015-11-08 20:04:19 -0500

    Added: static functions ShootFlat, Shoot, ShootM2, ShootArray. Also added public functions Fire, FireM2, FireArray.

--- BBT/Generation/Random/RandBreitWigner.js
+++ BBT/Generation/Random/RandBreitWigner.js
@@ -31,28 +31,149 @@
 
 */
 
-define( [], function(){
+define( [
+  '../BBT/Generation/Random/JamesRandom',
+], function( JamesRandom ){
   "use strict";
 
-  function RandBreitWigner( ){
-
+  function RandBreitWigner( args ){
+    this.fmean = args.mean || 1.0;
+    this.fgamma = args.gamma || 0.2;
+    this.fcut = args.cut || undefined;
+    this.fengine = args.engine || Object.create( JamesRandom() );
   } 
 
+  RandBreitWigner.Shoot = function( args ){
+    var smean = args.mean || 1.0;
+    var sgamma = args.gamma || 0.2;
+    var scut = args.cut || undefined;
+    var sengine = args.engine || Object.create( JamesRandom() );
+
+    if( sgamma == 0 ) return smean;
+
+    var rval, displ;
+    rval  = 2.0 * sengine.Flat() - 1.0;
+
+    if( scut === undefined ){
+      // Without cut
+      displ = 0.5 * sgamma * Math.tan( rval * Math.Pi * 0.5 ); 
+    } else {
+      // With cut
+      var val;
+      val = Math.atan( 2.0 * scut / sgamma );        
+      displ = 0.5 * gamma * Math.tan( rval * rval ); 
+    }
+    return ( smean + displ );
+  };
+
+  RandBreitWigner.ShootArray = function( args ){
+    var ssize = args.size || 1;
+    // var svect = args.vect;
+
+    var argsShoot = { mean: args.mean, gamma: args.gamma, cut: args.cut, engine: args.engine };
+
+    for( var i = 0; i < ssize; ++i ){
+      args.vect.push( RandBreitWigner.Shoot( argsShoot ) );
+    }
+    
+  };
 
 
+  // Tweak CLHEP C++ that used the RandFlat::shoot static method.
+  // Now this distribution has its own Shoot static function.  
+  RandBreitWigner.ShootFlat = function( args ){
+    var sa = args.a || 0;
+    var sb = args.b || 1;
+    var swidth = args.width || ( args.b - args.a );  
+    var sengine = args.engine || Object.create( JamesRandom() );
+    return ( swidth * sengine.Flat() + sa ); 
+  };
 
+  RandBreitWigner.ShootM2 = function( args ){
+    var sengine = args.engine || Object.create( JamesRandom() );
+    var smean = args.mean || 1.0;
+    var sgamma = args.gamma || 0.2;
+    var scut = args.cut || undefined;
+
+    var val, rval, displ;
+    if( sgamma == 0.0 ) return smean;
+
+    if( scut === undefined ){
+      // Without cut
+      val = Math.atan( -smean / sgamma );
+      rval = RandBreitWigner.ShootFlat( {  a: val, b: Math.PI / 2, engine: sengine  } );
+      displ = sgamma * Math.tan( rval );
+      return Math.sqrt( Math.pow( smean, 2 ) + smean * displ );          
+    } else {
+      // With cut
+      var lower, upper, tmp;
+
+      tmp = Math.max( 0.0, smean - scut );
+      lower = Math.atan( ( tmp * tmp - Math.pow( smean, 2) ) / ( smean * sgamma ) );
+
+      upper = Math.atan( ( Math.pow( smean + scut, 2 ) - Math.pow( smean, 2 ) ) / ( smean * sgamma ) );
+
+      rval = RandBreitWigner.ShootFlat( { a: lower, b: upper, engine: sengine } );
+
+      displ = sgamma * Math.tan( rval );
+
+      return Math.sqrt( Math.max( 0.0, Math.pow( smean, 2 ) + smean * displ ) );
+    }
+  };
 
   RandBreitWigner.prototype = {
     constructor: RandBreitWigner,
 
+    Fire: function(){
+      if( this.fgamma == 0 ) return this.fmean;
+      var rval, displ;
+      rval  = 2.0 * this.fengine.Flat() - 1.0;
 
+      if( this.fcut === undefined ){
+        // Without cut
+        displ = 0.5 * this.fgamma * Math.tan( rval * Math.Pi * 0.5 ); 
+      } else {
+        // With cut
+        var val;
+        val = Math.atan( 2.0 * this.fcut / this.fgamma );        
+        displ = 0.5 * gamma * Math.tan( rval * rval ); 
+      }
+      return ( this.fmean + displ );
+    },
 
+    FireM2: function(){
+      var val, rval, displ;
+      if( this.fgamma == 0.0 ) return this.fmean;
 
+      if( this.fcut === undefined ){
+        // Without cut
+        val = Math.atan( -this.fmean / this.fgamma );
+        rval = RandBreitWigner.ShootFlat( {  a: val, b: Math.PI / 2, engine: this.fengine  } );
+        displ = this.fgamma * Math.tan( rval );
+        return Math.sqrt( Math.pow( this.fmean, 2 ) + this.fmean * displ );          
+      } else {
+        // With cut
+        var lower, upper, tmp;
 
-  }
+        tmp = Math.max( 0.0, this.fmean - this.fcut );
+        lower = Math.atan( ( tmp * tmp - Math.pow( this.fmean, 2) ) / ( this.fmean * this.fgamma ) );
+
+        upper = Math.atan( ( Math.pow( this.fmean + this.fcut, 2 ) - Math.pow( this.fmean, 2 ) ) / ( this.fmean * this.fgamma ) );
+
+        rval = RandBreitWigner.ShootFlat( { a: lower, b: upper, engine: this.fengine } );
+
+        displ = this.fgamma * Math.tan( rval );
+
+        return Math.sqrt( Math.max( 0.0, Math.pow( this.fmean, 2 ) + this.fmean * displ ) );
+      }
+    },
+
+    FireArray: function( /* size of vect */ size, /* Array */ vect ){
+      for( var i = 0; i < size; ++i ){
+        vect.push( this.Fire() );
+      }
+    }
+  } // end prototype
 
   return RandBreitWigner;
-
 });
-
-
