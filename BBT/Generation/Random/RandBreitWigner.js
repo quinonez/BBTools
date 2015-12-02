a2daab3733354394d49511fed42a16df22a33ba3
Fernando Quinonez <fernando.andres.quinonez.granados@gmail.com>
2015-12-02 04:55:49 -0500

    Typo Corrected

--- BBT/Generation/Random/RandBreitWigner.js
+++ BBT/Generation/Random/RandBreitWigner.js
@@ -4,31 +4,22 @@
    |                       --- RandBreitWigner ---                        |
    |                            Module File                               |
    +----------------------------------------------------------------------+
-  
    Module defining methods for shooting numbers according to the
    Breit-Wigner distribution algorithms (plain or mean^2).
    Default values are set: mean=1, gamma=.2, cut=1.
    Plain algorithm is used for shootArray() and fireArray().
-  
-  
+   +----------------------------------------------------------------------+
+   | JavaScript                                                           |
+   +----------------------------------------------------------------------+
+   F. Quinonez - Created 2015-10-30                  
    +----------------------------------------------------------------------+
    | C++                                                                  |
    +----------------------------------------------------------------------+
-
    Gabriele Cosmo - Created: 5th September 1995
                   - Added methods to shoot arrays: 28th July 1997
    J.Marraffino   - Added default arguments as attributes and
                     operator() with arguments: 16th Feb 1998
    M Fischler      - put and get to/from streams 12/10/04
-
-  
-   +----------------------------------------------------------------------+
-   | JavaScript                                                           |
-   +----------------------------------------------------------------------+
-
-   F. Quinonez - Created 2015-10-30                  
-
-
 */
 
 define( [ '../Random/JamesRandom' ], function( JamesRandom ){
