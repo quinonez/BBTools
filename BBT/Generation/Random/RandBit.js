f2cda9466d68c0cde40ba8926ade8b99092be001
Fernando Quinonez <fernando.andres.quinonez.granados@gmail.com>
2015-12-04 18:10:29 -0500

    Shoot, ShootArray, Fire, FireArray functions added.

--- BBT/Generation/Random/RandBit.js
+++ BBT/Generation/Random/RandBit.js
@@ -4,17 +4,18 @@
    |                           --- RandBit ---                            |
    |                            Module File                               |
    +----------------------------------------------------------------------+
-  
    Module defining methods for shooting Flat or Bit random numbers, 
    double or integers.
    It provides methods to fill with double flat values arrays of
    specified size, as well as methods for shooting sequences of 0,1 (bits).
-  
-  
+   +----------------------------------------------------------------------+
+   | JavaScript                                                           |
+   +----------------------------------------------------------------------+
+   F. Quinonez - Created 2015-10-30                  
+               - Geant4 File
    +----------------------------------------------------------------------+
    | C++                                                                  |
    +----------------------------------------------------------------------+
-
    This is derived from RandFlat and is a drop-in replacement.  However
    the shootBit() and fireBit() methods are stateless (which makes them
    an order of magnitude slower, but allows save/restore engine status
@@ -23,50 +24,48 @@
    M Fischler     - Created from RandFlat.cc, deleting almost all the 
                     content since inheritance takes care of it.  2/15/00
    M Fischler     - put and get to/from streams 12/10/04
-  
-
-   +----------------------------------------------------------------------+
-   | JavaScript                                                           |
-   +----------------------------------------------------------------------+
-
-   F. Quinonez - Created 2015-10-30                  
-
-
 */
 
-define( [], function(){
+define([ '../Random/JamesRandom' ], function( JamesRandom ){
   "use strict";
 
-  function RandBit( engine, ){
-    this.fEngine = engine;
+  function RandBit( args ){
+    this.fengine = args.engine || Object.create( JamesRandom({}) );
   } 
 
+  RandBit.Shoot = function( args ){
+    var sengine = args.engine || Object.create( JamesRandom() );
+    var x = sengine.Flat();
+    var bit = ( x > 0.5 )? 1: 0;
+    return bit;
+  };
 
+  RandFlat.ShootArray = function( args ){
+    var ssize = args.size || 1;
+    var sengine = args.engine || Object.create( JamesRandom({}) );
+    // var svect = args.vect;
 
+    var argsShoot = { engine: sengine };
 
+    for( var i = 0; i < ssize; ++i ){
+      args.vect.push( RandFlat.Shoot( argsShoot ) );
+    }
+  };
 
   RandBit.prototype = {
     constructor: RandBit,
 
-
-    /*FIXME FIXME FIXME FIXME FIXME FIXME FIXME 
-    FIRST IMPLEMENT RandFlat.js
-    */
-    FireEndPoints: function( a, b ){
-      return ( ( b - a ) * this.fEngine.Flat() + a );
+    Fire: function(){
+      var x = this.fengine.Flat();
+      var bit = ( x > 0.5 )? 1: 0;
+      return bit;
     },
 
-    FireBit: function(){
-      var x = FireEndPoints( 0, 1 );
-      return ( ( x > 0.5 )? 1: 0 );
-    },
-
-    FireWidth: function( width ){
-      return ( width * this.fEngine.Flat() );
-    },
-
-
-
+    FireArray: function( /* size of vect */ size, /* Array */ vect ){
+      for( var i = 0; i < size; ++i ){
+        vect.push( this.Fire() );
+      }
+    }
   }
 
   return RandBit;
