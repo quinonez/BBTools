345c4a4718a91d8fed6fe707d7c321fbbe7e977e
Fernando Quinonez <fernando.andres.quinonez.granados@gmail.com>
2015-12-05 04:44:08 -0500

    Some changes.

--- BBT/Generation/Random/RandSkewNormal.js
+++ BBT/Generation/Random/RandSkewNormal.js
@@ -1,48 +1,54 @@
 /* 
    +----------------------------------------------------------------------+
    |                            HEP Random                                |
-   |                      --- RandSkewNormal ---                           |
+   |                     --- RandSkewNormal ---                           |
    |                            Module File                               |
    +----------------------------------------------------------------------+
-  
-  
-
-   +----------------------------------------------------------------------+
-   | C++                                                                  |
-   +----------------------------------------------------------------------+
-
-  
-
+   RandSkewNormal ---
+      returns a skew-normal distribution with shape parameter k
+      To get a distribution with scale parameter b and location m:
+      r = m + b * RandSkewNormal.Fire(k);
+   http://azzalini.stat.unipd.it/SN/
+   algorithm from K. McFarlane, June 2010.
    +----------------------------------------------------------------------+
    | JavaScript                                                           |
    +----------------------------------------------------------------------+
-
    F. Quinonez - Created 2015-10-30                  
-
-
+   +----------------------------------------------------------------------+
+   | C++                                                                  |
+   +----------------------------------------------------------------------+
+   M Fischler and L Garren - Created: 26 May 2011
 */
 
 define( [], function(){
   "use strict";
 
-  function RandSkewNormal( ){
+  function RandSkewNormal( args ){
+    this.fshape = args.shape || 0.0;
 
   } 
 
+  RandSkewNormal.GaussianSkewNormal = function( sengine, k ){
 
+  };
 
 
 
   RandSkewNormal.prototype = {
     constructor: RandSkewNormal,
 
+    GetShapeParameter: function(){
 
+    },
 
+    Fire: function(){
 
+    },
 
+    FireArray: function(){
+
+    }
   }
-
   return RandSkewNormal;
-
 });
 
